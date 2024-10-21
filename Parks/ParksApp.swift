1d2d3cd6f6af5b33556bcb650a147fa2c75a0688
benuhdrill <bgmach2023@fau.edu>
2024-10-21 16:18:57 -0400

    Initial Commit

---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Ben Gmach on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
