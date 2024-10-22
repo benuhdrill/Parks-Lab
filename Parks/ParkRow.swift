8869d175f1522d262b82173701a8c1919348d7ad
benuhdrill <bgmach2023@fau.edu>
2024-10-22 13:07:08 -0400

    Lab complete

---
+++ Parks/ParkRow.swift
@@ -0,0 +1,18 @@
+//
+//  ParkRow.swift
+//  Parks
+//
+//  Created by Ben Gmach on 10/21/24.
+//
+
+import SwiftUI
+
+struct ParkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParkRow()
+}
---
+++ Parks/ParksResponse.swift
@@ -0,0 +1,18 @@
+//
+//  ParksResponse.swift
+//  Parks
+//
+//  Created by Ben Gmach on 10/21/24.
+//
+
+import SwiftUI
+
+struct ParksResponse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParksResponse()
+}
